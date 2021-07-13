c46a6d3fb0c0a4b21efda94ea4926e9d766dbd35
Luczko <wlalukasz@gmail.com>
2021-07-13 20:32:36 +0200

    cssAdded

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import pizzas from "../data/pizzas.json";
 import Pizza from "./Pizza";
+import AppCSS from "./App.module.css";
 
 const App = () => {
   return (
-    <ul>
-      {pizzas.map((e) => (
-        <Pizza key={e.id} pizza={e} />
-      ))}
-    </ul>
+    <div className={AppCSS.container}>
+      <ul>
+        {pizzas.map((e) => (
+          <Pizza key={e.id} pizza={e} />
+        ))}
+      </ul>
+    </div>
   );
 };
 
